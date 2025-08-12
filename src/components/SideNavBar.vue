939b50eb80df48fdac737bfcdd4a5869cfa4bb75
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-08-12 12:57:57 +0800

    fix: update z-index of SideNavBar for proper stacking context

--- src/components/SideNavBar.vue
+++ src/components/SideNavBar.vue
@@ -62,7 +62,7 @@ onBeforeUnmount(()=>{
 $word-color: #2F2F2F;
 .sidNavBar{
     position: fixed;
-    z-index: 999; // 確保側邊導航欄在最上層
+    z-index: 1001; // 確保側邊導航欄在最上層
     top: 100px;
     left: 0;
     min-width: 90px;
