88a08d865dfb8f5eafb12932a924c785f63ed6a4
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-08 11:18:25 +0800

    fix: update list item alignment to use 'start' in Membership component

--- src/components/Membership.vue
+++ src/components/Membership.vue
@@ -143,10 +143,6 @@ overflow-y: auto;
                 }
             }
             ul{
-                display: flex;
-                flex-direction: column;
-                // align-items: center;
-                justify-content: start;
                 list-style: none;
                 text-align: left;
                 li{
