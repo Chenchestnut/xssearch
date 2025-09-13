58ee76273f18aa846932f2f32981a45f3a1c9ce5
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-13 15:38:49 +0800

    fix: update border-radius value for VS separator in CompareProductCard

--- src/components/CompareProductCard.vue
+++ src/components/CompareProductCard.vue
@@ -94,7 +94,7 @@ $word-color: #2F2F2F;
         font-size: 2rem;
         background-color: #a4aebd;
         border: #7e90a7 solid 1px;
-        border-radius: 50%;
+        border-radius: 50px;
         margin: 0 1.5rem;
         @media screen and (max-width: 520px) {
             font-size: 1.5rem;
