a3d7c219e28135c2f9f1806df3c15fc6125e571a
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-15 13:04:24 +0800

    fix: comment out text-align property in ReasonSection for improved layout consistency

--- src/components/ReasonSection.vue
+++ src/components/ReasonSection.vue
@@ -49,7 +49,7 @@ div.reason{
             margin: 1.5rem 0;
             p{
                 font-size: 1rem;
-                text-align: left;
+                // text-align: left;
             }
         }
     }
