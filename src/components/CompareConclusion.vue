98f7955e00ec183552d24941e1575cbc297fdd35
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-15 11:21:45 +0800

    fix: wrap title in CompareConclusion with a box for improved layout consistency

--- src/components/CompareConclusion.vue
+++ src/components/CompareConclusion.vue
@@ -4,7 +4,9 @@
 
 <template>
     <div class="compareConclusion">
-        <h2 class="title">結論</h2>
+        <div class="box">
+            <h2 class="title">結論</h2>
+        </div>
         <div class="content">
             <p>經過多方面的分析後，由iphone 15 pro max勝出，這款手機在性能、相機和電池續航方面表現出色，是一款值得推薦的產品。</p>
         </div>
@@ -24,11 +26,16 @@ hr{
     align-items: center;
     width: 100%;
     margin: 1rem 0;
-    h2.title{
-        font-size: 2rem;
-        font-weight: bold;
-        text-align: left;
-        // padding-top:1.5rem ;
+    div.box{
+        width: 100%;
+        display: flex;
+        justify-content: start;
+        h2.title{
+            font-size: 2rem;
+            font-weight: bold;
+            text-align: left;
+            // padding-top:1.5rem ;
+        }
     }
     div.content{
         background-color: #7E90A7;
