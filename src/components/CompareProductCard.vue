3cb097c7456bf9fdf754d53e3eb856515ae2e8b6
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-27 17:44:02 +0800

    fix: move StarDescription component inside star paragraph for better visibility

--- src/components/CompareProductCard.vue
+++ src/components/CompareProductCard.vue
@@ -36,10 +36,9 @@ function hideStarDescription(){
             </div>
             <div class="info">
                 <h2>iphone 15 pro max</h2>
-                <p class="star" @mouseenter="showStarDescription" @mouseleave="hideStarDescription"><i class="fa-regular fa-star"></i>4.0</p>
+                <p class="star" @mouseenter="showStarDescription" @mouseleave="hideStarDescription"><i class="fa-regular fa-star"></i>4.0<StarDescription class="starDescription" v-if="displayStarDescription" /></p>
             </div>
         </div>
-        <StarDescription class="starDescription" v-if="displayStarDescription" />
         <div class="vs">
             VS
         </div>
