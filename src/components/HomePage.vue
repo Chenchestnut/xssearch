a1862222484fed6771be62ac1e572e8ba0758a8c
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-09-29 11:58:19 +0800

    fix: enhance start button box-shadow for improved depth and visibility

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -248,7 +248,8 @@ overflow-x: hidden;
             transition: all 0.3s ease;
             color: $word-color;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            box-shadow: inset 8px 0px 8px rgba(255, 255, 255, 0.7);
+            box-shadow: inset 8px 8px 2px rgba(255, 255, 255, 0.7),
+                        inset -4px -4px 4px rgba(0, 0, 0, 0.1);
             @media screen and (max-width: 768px){
                 margin-top: 8rem;
             }
