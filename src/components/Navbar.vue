77d29772f6b8afbda883cb785b35aea88674a632
Chenchestnut <b11123040@gemail.yuntech.edu.tw>
2025-11-08 12:25:59 +0800

    fix: adjust right positioning of dropdown menu in Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -196,7 +196,7 @@ div.wholeNavbar{
                         min-width: 100px;
                         position: absolute;
                         top: 55px;
-                        right: -40px;
+                        right: -50px;
                         // left: 0;
                         margin-top: 2rem;
                         z-index: 999;
